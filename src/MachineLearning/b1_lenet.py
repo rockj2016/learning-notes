eabb8fc0d07e20dbea2189a82f6dfd25fee52850
y j <1044407513@qq.com>
2019-03-31 21:44:41 +0800

    .

--- src/MachineLearning/b1_lenet.py
+++ src/MachineLearning/b1_lenet.py
@@ -1,17 +1,18 @@
 import numpy as np
 
 
-def conv_forward(input_data, filters, bais=None, padding=0, stride=1):
+def conv_forward(input_data, filters, bias, padding=0, stride=1):
     """
     :param input_data: input  m x nc x iw x ih
     :param filters: filter nf x nc x fw x fh  n为filter数量
-    :param bais: bias 每一filter对应一个实数b nf x 1
+    :param bias: bias 每一filter对应一个实数b nf x 1
     :param padding: padding layer:l
     :param stride: stride layer:l
     :return:
     """
     m, nc, iw, ih = input_data.shape
     nf, nc, fw, fh = filters.shape
+
     ow, oh = (iw + 2*padding - fw) / stride + 1, (ih + 2*padding - fh) / stride + 1
     ow, oh = int(ow), int(oh)
 
@@ -23,12 +24,13 @@ def conv_forward(input_data, filters, bais=None, padding=0, stride=1):
         sample = padding_input_data[index_m]
         for index_w in range(ow):
             for index_h in range(oh):
-                temp = sample[:, index_w*stride:index_w*stride+fw, index_h*stride:index_h*stride+fh]    # 输入数据与filter对应的一个卷积块
+                sample_slice = sample[:, index_w*stride:index_w*stride+fw, index_h*stride:index_h*stride+fh]    # 输入数据与filter对应的一个卷积块
                 for index_f in range(nf):  # 遍历所有filter
-                    z[index_m, index_f, index_w, index_h] = np.sum(temp * filters[index_f] + bais[index_f])
+                    z[index_m, index_f, index_w, index_h] = np.sum(sample_slice * filters[index_f]) + bias[index_f][0]
+
     a = np.maximum(z, 0)
-    cache = (z, input_data, filters, bais, padding, stride)
-    # return a, cache
+
+    cache = (z, input_data, filters, bias, padding, stride)
     return a, cache
 
 
@@ -44,8 +46,9 @@ def conv_backword(da, cache):
     # print(z.shape,padding_a_1.shape)
 
     # relu 求导
+    z[z <= 0] = 0
     z[z > 0] = 1
-    dz = da*z
+    dz = da * z
 
     m,nf,w,h = dz.shape
     f = filters.shape[3]
@@ -56,11 +59,10 @@ def conv_backword(da, cache):
     db = np.zeros(bais.shape)
 
     padding_da_1 = np.pad(da_1, ((0, 0), (0, 0), (padding, padding), (padding, padding)), 'constant')
-
     for index_m in range(m):    # 遍历batch 样本
-        for index_w in range(w):
-            for index_h in range(h):
-                for index_c in range(nf): # 遍历dz的所有通道,也是遍历所有filter
+        for index_c in range(nf):
+            for index_w in range(w):
+                for index_h in range(h): # 遍历dz的所有通道,也是遍历所有filter
                     w_start = index_w * stride
                     w_end = index_w * stride + f
                     h_start = index_h * stride
@@ -68,34 +70,58 @@ def conv_backword(da, cache):
 
                     padding_da_1[index_m,:,w_start:w_end,h_start:h_end] += dz[index_m,index_c,index_w,index_h] * filters[index_c]
                     dw[index_c] += dz[index_m,index_c,index_w,index_h] * padding_a_1[index_m,:,w_start:w_end,h_start:h_end]
-
                     db[index_c] += dz[index_m,index_c,index_w,index_h]
+        if padding:
+            da_1[index_m, :, :, :] = padding_da_1[index_m,:,padding:-padding,padding:-padding]
+        else:
+            da_1[index_m, :, :, :] = padding_da_1[index_m]
 
-        da_1[index_m, :, :, :] = padding_da_1[index_m,:,padding:-padding,padding:-padding]
     return da_1, dw, db
 
 
-def test_conv():
-    np.random.seed(1)
-    # A_prev -- output activations of the previous layer, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
-    # A_prev = np.random.randn(10, 4, 4, 3)
-    # A_prev = np.random.randn(10, 3, 4, 4)
-    A_prev = np.ones((10, 3, 4, 4))
-    #  W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
-    # W = np.random.randn(2, 2, 3, 8)
-    # W = np.random.randn(8, 3, 2, 2)
-    W = np.ones((8, 3, 2, 2))
-    b = np.random.randn(8,1)
+# def test_conv_2():
+#     # np.random.seed(1)
+#     input = np.random.randn(10,1,28,28).astype(np.float64)
+#     w = np.random.randn(6,1,5,5).astype(np.float64)
+#     b = np.random.randn(6,1).astype(np.float64)
+#     y = np.ones((10,6,24,24)).astype(np.float64)
+#     for i in range(50):
+#         print('-'*50)
+#         z, cache = conv_forward(input, w, b)
+#         loss = np.mean(np.square(z - y))
+#         print(loss)
+#         dz = z-y
+#         _, dw, db = conv_backword(dz, cache)
+#         # print(dw)
+#         # print(b)
+#         # print(db)
+#         w -= dw * 0.001
+#         b -= db * 0.001
 
-    z, cache_conv = conv_forward(A_prev, W, b, 2,1)
-    # print(Z[0][0])
-    print("Z's mean =", np.mean(z))
-    # print("cache_conv[0][1][2][3] =", cache_conv[0][1][2][3])
-    np.random.seed(1)
-    da_1, dw, db = conv_backword(z, cache_conv)
-    print("dA_mean =", np.mean(da_1))
-    print("dW_mean =", np.mean(dw))
-    print("db_mean =", np.mean(db))
+
+# test_conv_2()
+
+
+# def test_conv():
+#     np.random.seed(1)
+#     x = np.array([x for x in range(1,17)] + [x for x in range(1,17)]).reshape(1,2,4,4)
+#     w = np.ones((1, 2, 2, 2)) * 0.2
+#     b = np.ones((1, 1))
+#     print(x)
+#     print("\n")
+#
+#     a, cache_conv = conv_forward(x, w, b)
+#
+#     # print(a.shape)
+#     print("\n")
+#     testa = np.ones((1,1,3,3))
+#     print("\n")
+#     da_1, dw, db = conv_backword(testa, cache_conv)
+#     print(db)
+#     print("\n")
+#
+#     print(dw)
+#     print("\n")
 # test_conv()
 
 
@@ -159,30 +185,316 @@ def pool_backward(da, cache):
     return da_prev
 
 
-def test_pool():
-    np.random.seed(1)
-    # A_prev -- Input data, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
-    # A_prev = np.random.randn(2, 4, 4, 3)
-    A_prev = np.random.randn(1, 1, 4, 4)
-    print(A_prev)
-    pool = 2
-    stride = 2
-    res, cache = pool_forward(A_prev,pool,stride,'max')
-    print(res)
-    print('-'*50)
-    res = pool_backward(res,cache)
-    print(res)
-test_pool()
+# def test_pool():
+#     np.random.seed(1)
+#     # A_prev -- Input data, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+#     # A_prev = np.random.randn(2, 4, 4, 3)
+#     A_prev = np.random.randn(1, 1, 6, 6)
+#     A_prev = np.array(range(1,37)).reshape(1, 1, 6, 6)
+#
+#     pool = 2
+#     stride = 2
+#     res, cache = pool_forward(A_prev,pool,stride,'avg')
+#     print(res)
+#     print('-'*50)
+#     # res = np.ones((1, 1, 3, 3))
+#     res = pool_backward(res,cache)
+#     print(res)
+#     print(res.shape)
+# test_pool()
 
 
+def fc_forward(ai_1, w, b, mode):
+    """
+    :param ai_1: ai-1
+    :param w: wi
+    :param b: bi
+    :return: ai-1, zi, ai, wi, bi
+    """
+    z = np.dot(w, ai_1) + b
+    if mode == 'relu':
+        ai = np.maximum(z, 0)
+    else :
+        if z.shape[1] == 1:
+            exp = np.exp(z - np.max(z))
+            ai = exp / np.sum(exp)
+        else:
+            exp = np.exp(z - np.max(z, axis=0, keepdims=True))
+            ai = exp / np.sum(exp, axis=0, keepdims=True)
+    cache = (ai_1, z, w)
+    return ai, cache
 
 
+def fc_backward(da, cache, dz=None):
+    """
+    :param da: dai 第 i 层 da 用于求dz
+    :param dz: dzi 如传入dzi则函数内不再根据da,z计算dzi
+    :return: dai-1 用于前一层求导计算
+    """
+    ai_1, z, w = cache
+    if dz is None:
+        z[z <= 0] = 0
+        z[z > 0] = 1
+        dz = da * z.T
+    dw = np.dot(dz.T, ai_1.T)
+    db = np.sum(dz.T, axis=1, keepdims=True)
+
+    dai_1 = np.dot(dz, w)
+
+    return dw,db,dai_1
 
 
+def get_data():
+    with open('../data/mnist_train.csv', 'r') as f:
+        data = [x.strip().split(',') for x in f]
+        data = np.asarray(data, dtype='float').T
+        # print(data[0][:10])
+        y = np.zeros((10, len(data[0])))
+        for i, x in enumerate(data[0]):
+            y[:, i][int(x)] = 1
+        x = np.delete(data, 0, 0)
+        x = (x / 255.0 * 0.99) + 0.01
+        x = x - x.mean(axis=1, keepdims=True)
+        x = x / np.sqrt(x.var(axis=1, keepdims=True) + 10e-8)
+        return x, y
 
 
+def get_test_data():
+    with open('../data/mnist_test.csv', 'r') as f:
+        data = [x.strip().split(',') for x in f]
+        data = np.asarray(data, dtype='float').T
+        # print(data[0][:10])
+        y = data[0]
+        x = np.delete(data, 0, 0)
+        x = ((x / 255.0) * 0.99) + 0.01
+        x = x - x.mean(axis=1, keepdims=True)
+        x = x / np.sqrt(x.var(axis=1, keepdims=True) + 10e-8)
+        return x, y
 
 
+class LeNet:
+    def __init__(self,learning_rate):
+        self.batch_size = 32
+        self.w_a = np.random.randn(6, 1, 5, 5)
+        self.b_a = np.random.randn(6, 1)
+        self.vdw_a = np.zeros(self.w_a.shape)
+        self.vdb_a = np.zeros(self.b_a.shape)
 
+        self.w_c = np.random.randn(16, 6, 5, 5)
+        self.b_c = np.random.randn(16, 1)
+        self.vdw_c = np.zeros(self.w_c.shape)
+        self.vdb_c = np.zeros(self.b_c.shape)
+
+        self.w_e = np.random.randn(120, 256) * np.sqrt(2 / 256)
+        self.b_e = np.zeros((120,1))
+        self.vdw_e = np.zeros(self.w_e.shape)
+        self.vdb_e = np.zeros(self.b_e.shape)
+
+        self.w_f = np.random.randn(84, 120) * np.sqrt(2 / 120)
+        self.b_f = np.zeros((84, 1))
+        self.vdw_f = np.zeros(self.w_f.shape)
+        self.vdb_f = np.zeros(self.b_f.shape)
+
+        self.w_g = np.random.randn(10, 84) * np.sqrt(2 / 84)
+        self.b_g = np.zeros((10, 1))
+        self.vdw_g = np.zeros(self.w_g.shape)
+        self.vdb_g = np.zeros(self.b_g.shape)
+
+        self.x, self.y = self.get_batch_data()
+        self.learning_rate = learning_rate
+        self.beta = 0.9
+
+    @staticmethod
+    def cross_entropy(p, y, epsilon=1e-12):
+        p = np.clip(p, epsilon, 1. - epsilon)
+        m = p.shape[1]
+        ce = -np.sum(y * np.log(p)) / m
+        return ce
+
+    def get_batch_data(self):
+        x, y = get_data()
+        m = x.shape[1]
+        batch_split_index = []
+        index = self.batch_size
+        while index <= m:
+            batch_split_index.append(index)
+            index += self.batch_size
+        x_batches = np.split(x, batch_split_index, axis=1)
+        y_batches = np.split(y, batch_split_index, axis=1)
+        for index in range(len(x_batches)):
+            batch_size = x_batches[index].shape[1]
+            x_batches[index] = x_batches[index].T.reshape(batch_size,1,28,28)
+        return x_batches,y_batches
+
+    def train(self,iteration):
+        beta = self.beta
+        for epoch in range(1,iteration+1):
+            print(epoch)
+            for x, y in zip(self.x, self.y):
+                # --- forward -----
+                # layer a convolution layer
+                # input :  (batch-size, 1, 28, 28)
+                # padding : 1
+                # stride : 1
+                # filters : (6, 1, 5, 5)
+                # b : (6, 1)
+                # output : (batch-size, 6, 24, 24)
+                a_a, cache_a = conv_forward(x, self.w_a, self.b_a, padding=0, stride=1)
+
+                # layer b pooling layer
+                # input : (batch-size, 6, 24, 24)
+                # stride : 2
+                # filter : (2,2)
+                # output : (batch-size, 6, 12, 12)
+                # pool_forward(input_data, pool_size, stride, mode='max')
+                a_b, cache_b = pool_forward(a_a, 2, 2, mode='max')
+
+                # layer c convolution layer
+                # input : (batch-size, 6, 12, 12)
+                # padding : 1
+                # stride : 1
+                # filters : (16, 6, 5, 5)
+                # b : (16, 1)
+                # output : (batch-size, 16, 8, 8)
+                a_c, cache_c = conv_forward(a_b, self.w_c, self.b_c, padding=0, stride=1)
+
+                # layer d pooling layer
+                # input : (batch-size, 16, 8, 8)
+                # stride : 2
+                # filter : (2,2)
+                # output : (batch-size, 16, 4, 4)
+                a_d, cache_d = pool_forward(a_c, 2, 2, mode='max')
+
+                # reshape output (batch-size, 256)
+
+                batch_size = a_d.shape[0]
+                a_d_reshape = a_d.reshape(batch_size,256).T
+
+                # layer e fully connected layer
+                # input : (256, batch_size) # batch里面每个 sample 的所有 features 占一列
+                # w : (120,256)
+                # b : (120,1)
+                # output :(120, batch_size)
+                a_e, cache_e = fc_forward(a_d_reshape,self.w_e,self.b_e,'relu')
+
+                # layer f fully connected layer
+                # input : (120, batch_size)
+                # w : (84, 120)
+                # b : (84, 1)
+                # output : (84,batch-size)
+                a_f, cache_f = fc_forward(a_e, self.w_f, self.b_f,'relu')
+
+                # layer g output with softmax
+                # input : (batch-size, 84)
+                # w : (10, 84)
+                # b : (10, 1)
+                # output : (batch-size, 10)
+                a_g, cache_g = fc_forward(a_f, self.w_g, self.b_g, 'softmax')
+
+                loss = self.cross_entropy(a_g,y)
+                print(f'loss : {loss}')
+
+                # --- backward ---
+
+                dz_g = (a_g - y).T / batch_size
+                dw_g, db_g, da_f = fc_backward(None, cache_g, dz_g)
+
+                # self.w_g -= dw_g * self.learning_rate
+                # self.b_g -= db_g * self.learning_rate
+                vdw_g = beta * self.vdw_g + (1 - beta) * dw_g
+                vdb_g = beta * self.vdb_g + (1 - beta) * db_g
+                self.vdw_g = vdw_g
+                self.vdb_g = vdb_g
+                self.w_g -= vdw_g * self.learning_rate
+                self.b_g -= vdb_g * self.learning_rate
+
+                # layer f fully connected layer
+                # input : (120.batch_size)
+                # w : (84, 120)
+                # b : (84, 1)
+                # output : (84,batch-size)
+                dw_f, db_f, da_e = fc_backward(da_f, cache_f, None)
+
+                # self.w_f -= dw_f * self.learning_rate
+                # self.b_f -= db_f * self.learning_rate
+                vdw_f = beta * self.vdw_f + (1 - beta) * dw_f
+                vdb_f = beta * self.vdb_f + (1 - beta) * db_f
+                self.vdw_f = vdw_f
+                self.vdb_f = vdb_f
+                self.w_f -= vdw_f * self.learning_rate
+                self.b_f -= vdb_f * self.learning_rate
+
+                # layer e fully connected layer
+                # input : (256,batch_size) # batch里面每个 sample 的所有 features 占一列
+                # w : (120,256)
+                # b : (120,1)
+                # output :
+                dw_e, db_e, da_d_reshape = fc_backward(da_e, cache_e, None)
+
+                # self.w_e -= dw_e * self.learning_rate
+                # self.b_e -= db_e * self.learning_rate
+                vdw_e = beta * self.vdw_e + (1 - beta) * dw_e
+                vdb_e = beta * self.vdb_e + (1 - beta) * db_e
+                self.vdw_e = vdw_e
+                self.vdb_e = vdb_e
+                self.w_e -= vdw_e * self.learning_rate
+                self.b_e -= vdb_e * self.learning_rate
+
+                da_d = da_d_reshape.reshape(batch_size, 16, 4, 4)
+
+                # layer d pooling layer
+                # input : (batch-size, 16, 8, 8)
+                # stride : 2
+                # filter : (2,2)
+                # output : (batch-size, 16, 4, 4)
+                da_c = pool_backward(da_d,cache_d)
+
+                # layer c convolution layer
+                # input : (batch-size, 6, 12, 12)
+                # padding : 1
+                # stride : 1
+                # filters : (16, 6, 5, 5)
+                # b : (16, 1)
+                # output : (batch-size, 16, 8, 8)
+                da_b,dw_c,db_c = conv_backword(da_c,cache_c)
+
+                # self.w_c -= dw_c * self.learning_rate
+                # self.b_c -= db_c * self.learning_rate
+                vdw_c = beta * self.vdw_c + (1 - beta) * dw_c
+                vdb_c = beta * self.vdb_c + (1 - beta) * db_c
+                self.vdw_c = vdw_c
+                self.vdb_c = vdb_c
+                self.w_c -= vdw_c * self.learning_rate
+                self.b_c -= vdb_c * self.learning_rate
+
+                # layer b pooling layer
+                # input : (batch-size, 6, 24, 24)
+                # stride : 2
+                # filter : (2,2)
+                # output : (batch-size, 6, 12, 12)
+                # pool_forward(input_data, pool_size, stride, mode='max')
+                da_a = pool_backward(da_b, cache_b)
+
+                # layer a convolution layer
+                # input :  (batch-size, 1, 28, 28)
+                # padding : 1
+                # stride : 1
+                # filters : (6, 1, 5, 5)
+                # b : (6, 1)
+                # output : (batch-size, 6, 24, 24)
+                da_x, dw_a, db_a = conv_backword(da_a, cache_a)
+                # self.w_a -= dw_a * self.learning_rate
+                # self.b_a -= db_a * self.learning_rate
+                vdw_a = beta * self.vdw_a + (1 - beta) * dw_a
+                vdb_a = beta * self.vdb_a + (1 - beta) * db_a
+                self.vdw_a = vdw_a
+                self.vdb_a = vdb_a
+                self.w_a -= vdw_a * self.learning_rate
+                self.b_a -= vdb_a * self.learning_rate
+
+
+if __name__ == '__main__':
+    net = LeNet(0.0005)
+    net.train(10)
 
 
